d8001a46608c9a3cc64d2bed5ce36b2f239ff869
HBK <sammcowen@gmail.com>
2022-03-18 14:54:23 -0700

    created routes for all tag-routes in tag-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
+    attributes:['id','category_name'],
     include: [
       {
         model: Product,
