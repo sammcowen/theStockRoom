e30272e924808f29a4713eb0fd748a5b219ace57
HBK <sammcowen@gmail.com>
2022-03-17 17:40:44 -0700

    trying to connect sequelize db

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,25 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    // id column being created accepts integer input ,not allowed
+    //  to be null, primary key,auto increments.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define category name column accepts input as string and cannot be null
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
