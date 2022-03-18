f01ec86f7efd380f15121a30444bc5460542a2ee
HBK <sammcowen@gmail.com>
2022-03-17 19:24:31 -0700

    created db  models for product, producttag, tag, and category

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,41 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-   
+    // define id column as primary key auto increment and cant be null
+   id: {
+     type:DataTypes.INTEGER,
+     allowNull:false,
+     primaryKey:true,
+     autoIncrement:true,
+   },
+  // define  product_name column as as tring and cannot be null
+   product_name:{
+     type:DataTypes.STRING,
+     allowNull:false
+   },
+  //  define price column as decimal , not null and validate it as a decimal
+   price:{
+     type:DataTypes.DECIMAL(6,2),
+     allowNull:false,
+     validate:{ 
+       isDecimal:true
+     }
+   },
+   stock:{
+     type:DataTypes.INTEGER,
+    allowNull:false,
+    defaultValue:10,
+    validate:{
+      isNumeric:true
+    }
+   },
+   category_id:{
+     type:DataTypes.INTEGER,
+     references:{
+       model:'category',
+       key:'id'
+     }
+   }
   },
   {
     sequelize,
--- server.js
+++ server.js
@@ -17,7 +17,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
-// // sequelize.sync({force: false}).then (()=> {
-// app.listen(PORT, () => 
-//   console.log(`App listening on port ${PORT}!`));
-// // });
+
