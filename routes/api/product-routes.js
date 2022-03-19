17b21f59cb7839055a501de353da8e47bf1f4b47
HBK <sammcowen@gmail.com>
2022-03-18 19:07:54 -0700

    fixed error in delete route for product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -130,7 +130,7 @@ router.put('/:id', (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
+       console.log(err);
       res.status(400).json(err);
     });
 });
@@ -143,11 +143,11 @@ router.delete('/:id', (req, res) => {
     }
   })
     .then(productdata => {
-      if (!productData) {
+      if (!productdata) {
         res.status(404).json({ message: 'no product found with that id' });
         return;
       }
-      res.json(productdata)
+      res.json(productdata);
     })
     .catch(err => {
       console.log(err)
